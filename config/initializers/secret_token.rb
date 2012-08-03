72ccffd7133434febcc3b04577d89b3246df247e
Nir Naor <nirnaori@gmail.com>
2012-08-03 13:34:44 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kr8tivcollection::Application.config.secret_token = 'c4ebaa5d6b1b5b12fad9dcda707a546cb74567cb16223bc2dfddb473f5096de405eee3b4411ad1a6a92dab30000568ddd7588f3869c53f2404f5016ba5cca961'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Kr8tivcollection::Application.config.session_store :cookie_store, key: '_kr8tivcollection_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Kr8tivcollection::Application.config.session_store :active_record_store
