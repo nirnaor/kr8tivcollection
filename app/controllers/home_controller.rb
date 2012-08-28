04216784165badcbfd5da1b9605e5711ee825de4
Nir Naor <nirnaori@gmail.com>
2012-08-28 16:34:32 +0300

    added crossslide to the app

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
+    gon.jewels = Jewel.find_all_by_show_in_home_page(true).map do |jewel|
+      {
+        alt: jewel.name,
+        src: jewel.primary_image.url,
+        from: 'top left',
+        to:   'bottom right 1.5x',
+        time: 2
+      }
+    end
   end
 end
