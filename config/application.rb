816d8087eb492aa6188e047747ddb4afeedd2f58
Nir Naor <nirnaori@gmail.com>
2012-09-26 02:17:12 +0200

    not accessing db when precompiling assests

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ if defined?(Bundler)
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
-
+config.assets.initialize_on_precompile = false
 module Kr8tivcollection
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
